0b7e531c021445fceaf495a7c9c6f462f31382d0
keith <1033610335@qq.com>
2025-07-21 12:41:34 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -16,7 +16,7 @@ export default defineUserConfig({
             '/bypy/',
             '/supervisor/',
             '/bangumi-data/',
-            '/adb'
+            '/adb/'
         ],
         colorMode: 'auto', //auto 为自动 dark 为黑暗模式
         colorModeSwitch: true,
